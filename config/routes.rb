6468435c7a5db7292727adb2830edfe59524bad0
Manoj Naidu <manojnaidu619@gmail.com>
2018-06-27 13:29:40 +0530

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   resources :pages
-  resources :category_filters
+  resources :category_filters, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
